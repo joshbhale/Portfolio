82c80e12d2c055473a2b1e6f0a13dc3a542e439a
joshbhale <joshbhale@gmail.com>
2024-10-28 14:04:12 -0400

    recent work section modified

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// Start coding
+
+class CssPropControl {
+  constructor(element) {
+    this.element = element
+  }
+  get(varName) {
+    return getComputedStyle(this.element).getPropertyValue(varName)
+  }
+  set(varName, val) {
+    return this.element.style.setProperty(varName, val)
+  }
+}
+
+const bodyCssProps = new CssPropControl(document.body)
+
+let toggle = document.querySelector('#dark-mode-toggle')
+toggle.addEventListener('click', () => { 
+  let mode = toggle.checked ? 'dark' : 'light'
+  bodyCssProps.set('--background', bodyCssProps.get(`--${mode}-background`))
+  bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`))
+  bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
+})
